e77254f1ce0f18d8e93bf751d04fee9cd53e6881
yagpdc <ysantanaf@gmail.com>
2024-08-11 21:46:47 -0300

    add: vetor homepage

--- app/src/Helpers/membros.tsx
+++ app/src/Helpers/membros.tsx
@@ -1,7 +1,13 @@
 import { StaticImageData } from "next/image"
 import Yago from "../../../public/membros/yago.jpg"
 import Toninho from "../../../public/membros/toninho.jpg"
-import Placeholder from "../../../public/membros/placeholder-profile.png"
+import Placeholder0 from "../../../public/membros/placeholders/placeholder0.png"
+import Placeholder1 from "../../../public/membros/placeholders/placeholder1.jpeg"
+import Placeholder2 from "../../../public/membros/placeholders/placeholder2.jpg"
+import Placeholder3 from "../../../public/membros/placeholders/placeholder3.jpg"
+import Placeholder4 from "../../../public/membros/placeholders/placeholder4.jpg"
+import Placeholder5 from "../../../public/membros/placeholders/placeholder5.jpg"
+import Placeholder6 from "../../../public/membros/placeholders/placeholder6.jpg"
 
 interface MembersObject {
     nome: string,
@@ -13,33 +19,34 @@ interface MembersObject {
 const membros_data: MembersObject[] =[
     {
         nome: "Yago",
-        imagem: Placeholder,
+        imagem: Placeholder0,
         data: "02/01/1997",
         setor: "coordenação"
     },
     {
-        nome: "Yago",
-        imagem: Placeholder,
-        data: "02/01/1997",
-        setor: "apostila"
-    },
-    {
-        nome: "Yago",
-        imagem: Placeholder,
-        data: "02/01/1997",
-        setor: "apostila"
-    },
-    {
-        nome: "douglas",
-        imagem: Placeholder,
+        nome: "Clara",
+        imagem: Placeholder1,
         data: "02/01/1997",
         setor: "desenvolvimento"
+    },
+
+    {
+        nome: "Silvia",
+        imagem: Placeholder2,
+        data: "02/01/1997",
+        setor: "apostila"
+    },
+    {
+        nome: "Clara",
+        imagem: Placeholder1,
+        data: "02/01/1997",
+        setor: "apostila"
     },
     {
         nome: "Toninho",
         imagem: Toninho,
         data: "02/01/1997",
-        setor: "desenvolvimento"
+        setor: "apostila"
     },
 
     {
@@ -49,62 +56,62 @@ const membros_data: MembersObject[] =[
         setor: "desenvolvimento"
     },
     {
-        nome: "Yago",
-        imagem: Placeholder,
+        nome: "Pedro",
+        imagem: Placeholder4,
         data: "02/01/1997",
         setor: "material"
     },
     {
-        nome: "Yago",
-        imagem: Placeholder,
+        nome: "Silvia",
+        imagem: Placeholder2,
         data: "02/01/1997",
         setor: "material"
     },
     {
-        nome: "Yago",
-        imagem: Placeholder,
+        nome: "Douglas",
+        imagem: Placeholder5,
         data: "02/01/1997",
         setor: "desenvolvimento"
     },
     {
-        nome: "Yago",
-        imagem: Placeholder,
+        nome: "Ana",
+        imagem: Placeholder6,
+        data: "02/01/1997",
+        setor: "instrutor"
+    },
+    {
+        nome: "Douglas",
+        imagem: Placeholder5,
+        data: "02/01/1997",
+        setor: "instrutor"
+    },
+    {
+        nome: "Clara",
+        imagem: Placeholder1,
         data: "02/01/1997",
         setor: "instrutor"
     },
     {
         nome: "Yago",
-        imagem: Placeholder,
-        data: "02/01/1997",
-        setor: "instrutor"
-    },
-    {
-        nome: "Yago",
-        imagem: Placeholder,
-        data: "02/01/1997",
-        setor: "instrutor"
-    },
-    {
-        nome: "Yago",
-        imagem: Placeholder,
+        imagem: Placeholder0,
         data: "02/01/1997",
         setor: "marketing"
     },
     {
-        nome: "Yago",
-        imagem: Placeholder,
+        nome: "Silvia",
+        imagem: Placeholder2,
         data: "02/01/1997",
         setor: "marketing"
     },
     {
-        nome: "Yago",
-        imagem: Placeholder,
+        nome: "Bruna",
+        imagem: Placeholder3,
         data: "02/01/1997",
         setor: "marketing"
     },
     {
-        nome: "Yago",
-        imagem: Placeholder,
+        nome: "Clara",
+        imagem: Placeholder1,
         data: "02/01/1997",
         setor: "desenvolvimento"
     },
--- app/src/components/_Homepage/_About/Main/index.tsx
+++ app/src/components/_Homepage/_About/Main/index.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import styles from "../Main/styles.module.css";
 import Carousel from "../Carousel";
 import cards from "../../../../Helpers/cards";
+import Image  from 'next/image';
 
 function About() {
   return (
     <div className={styles.about}>
       <div className={styles.content}>
+        <div className={styles.img_vetor}>
+          <Image src='/menina_homepage.svg' height={600} width={600} alt='menina vetor'></Image>
+        </div>
         <div className={styles.carousel}>
           <Carousel cards={cards} />
         </div>
